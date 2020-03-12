ca740b2fa3bd2f0a906b0488db262d5b9bab81c1
pachopascolat <patriciopascolat@gmail.com>
2020-03-12 10:07:10 -0300

    url a la noticia

--- config/web.php
+++ config/web.php
@@ -69,6 +69,7 @@ $config = [
             'showScriptName' => false,
             'rules' => [
                 '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                '<controller:\w+>/<id:\d+>' => '<controller>/news-view',
                 '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
                 '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
                 'admin' => 'inicio-carousel/index',
