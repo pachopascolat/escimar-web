6e142629b537fba0a4663f67af0ffcc9f93b30cf
pachopascolat <patriciopascolat@gmail.com>
2020-03-12 09:55:48 -0300

    url a la noticia

--- controllers/NoticiasController.php
+++ controllers/NoticiasController.php
@@ -86,7 +86,7 @@ class NoticiasController extends Controller {
         $pageFeed = "/me/feed";
 
         $pagTitulo = "Dale Boca !!!!!!";
-        $pagURL = \yii\helpers\Url::base(TRUE)."/$model->image_path";
+        $pagURL = \yii\helpers\Url::base(TRUE).\yii\helpers\Url::to(['news-view','id'=>$id]);
 
         $fb = new Facebook([
             'app_id' => $appid,
