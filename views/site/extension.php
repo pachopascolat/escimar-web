d9fced298368d4813f2e39f1fd39a0d273deabb6
norberto <norberto@norberto>
2020-03-13 18:42:42 -0300

    vista de extension

---
+++ views/site/extension.php
@@ -0,0 +1,150 @@
+<?php
+
+use app\assets\FrontAsset;
+
+$this->params['frontend'] = true;
+FrontAsset::register($this);
+?>
+<div class="institucional-page">
+    <div class="row " id="institucional-banner">
+
+            <div class="d-flex justify-content-center align-items-center w-100">
+                <div> 
+                    <h1 class=" bg-title-blue text-light  p-3 text-center">
+                        Proyectos de Extensión de la Escuela Superior de Ciencias Marinas
+                    </h1>
+                </div>
+
+            </div>
+
+    </div>
+    <div class="container pt-5 pb-5">
+        <div>
+            <div class="bg-biologia-blue p-2 mb-3 d-flex align-items-center">
+                <h4 class="text-light mb-0">Ejecución Año 2016 (Aprobado por Ordenanza 511/2016)</h4>
+            </div>
+            <div>
+                <br>
+            <!--<i class="fad fa-chevron-double-right mr-1 text-biologia-blue fa-2x"></i>-->
+                <h4>Proyecto: "Programa de capacitación y transferencia tecnológica en técnicas de producción de semillas de moluscos bivalvos en criadero."</h4>
+                <p>
+                    Directora: Marcela PASCUAL <br>
+                    Duración: 1año <br>
+                    Objetivo General: Lograr difundir la actividad de acuicultura a través de los
+                    medios disponibles en la Universidad, generando una oferta anual y estable de
+                    formación de recursos humanos especializados en mari cultura de criaderos. 
+                    Trasmitir los conocimientos y tecnologías relativas a la producción en criadero 
+                    de semillas de moluscos marinos a través de capacitaciones grupales e individuales.
+                </p>
+                <br>
+                
+                <h4>Proyecto: "El Mar en la Meseta."</h4>
+                <p>
+                    Directora: Maria Andrea de las Mercedes ROCHE <br>
+                    Duración: 1 año <br>
+                    Objetivo General: Brindar a los niños la posibilidad de acercarse al mar y su fauna
+                    de la mano de la ciencia de una forma diferente: accesible y divertida. Ampliando así
+                    sus conocimientos en el área de las ciencias naturales y sociales e integrando dos 
+                    niveles educativos muy diferentes.
+                </p>
+                <br>
+                
+                <h4>Proyecto: "Inocuidad y calidad en productos de la industria alimentaria de manufactura industrial y artesanal."</h4>
+                <p>
+                    Directora: Elba D'AMICO <br>
+                    Duración: 1año <br>
+                    Objetivo General: Este Proyecto pretende mejorar, a través de la incorporación de 
+                    contenidos conceptuales y prácticas específicas, la calidad e inocuidad de los productos 
+                    alimenticios que se elaboran en la región, logrando seguridad alimentaria.
+                </p>
+                <br>
+         
+            </div>
+        </div>
+        <hr>
+
+        <div>
+            <div class="bg-biologia-blue p-2 mb-3 d-flex align-items-center">
+                <h4 class="text-light mb-0">Ejecución Año 2017 y 2018 (Aprobado por Ordenanza 877/2017)</h4>
+            </div>
+            <div>
+                <br>
+            <!--<i class="fad fa-chevron-double-right mr-1 text-biologia-blue fa-2x"></i>-->
+                <h4>Proyecto: "Tecnología de elaboración de productos pesqueros para instituciones educativas y comunidad." </h4>
+                <p>
+                    Directora: Elba Noemi D'AMICO <br>
+                    Co-Directora: María Ignacia SICA <br>
+                    Duración: 2 años <br>
+                    Objetivo General: Este proyecto pretende mejorar, a través de la incorporación de contenidos 
+                    conceptuales y prácticas específicas, la calidad e inocuidad de los productos que se elaboran 
+                    en la región y brindar información sobre técnicas innovadoras para ampliar la oferta actual e 
+                    incentivar el consumo. 
+                </p>
+                <br>
+                
+                
+                <h4>Proyecto: "La escuela a la escuela." </h4>
+                <p>
+                    Directora: María Paula FAVA <br>
+                    Co-Director: Matías Simón MAGGIONI <br>
+                    Duración: 2 años <br>
+                    Objetivo General: Brindar a los niños la posibilidad de acercarse a la química, la física y al 
+                    mar de la mano de la ciencia de una forma diferente: accesible y divertida. Ampliando así sus 
+                    conocimientos en el área de las ciencias naturales y sociales e integrando dos niveles educativos 
+                    muy diferentes. Motivar el interés y la protección del mar y su entorno, utilizando al mar como 
+                    recurso para visualizar conceptos claves de la química y la física. 
+                </p>
+                <br>
+                
+                <h4>Proyecto: "Programa de capacitación y transferencia tecnológica en técnicas de producción de semillas de moluscos bivalvos en criadero y cultivo de zooplanton." </h4>
+                <p>
+                    Director: Eduardo ZAMPATI <br>
+                    Co-Directora: Myriam EL VIRA <br>
+                    Duración: 2 años <br>
+                    Objetivo General: Lograr difundir la actividad de acuicultura a través de los medios disponibles 
+                    en la Universidad, generando una oferta anual y estable de formación de recursos humanos especializados 
+                    en Maricultura de criaderos. Transmitir los conocimientos y tecnologías relativas a la producción 
+                    en criadero de semillas de moluscos marinos y cultivos de zooplancton a través de capacitaciones grupales 
+                    e individuales. 
+                </p>
+                <br>
+                
+            </div>
+        </div>
+        <hr>
+        <div>
+            <div class="bg-biologia-blue p-2 mb-3 d-flex align-items-center">
+                <h4 class="text-light mb-0">Ejecución Año 2018 (Aprobado por Ordenanza 1078/2018)</h4>
+            </div>
+            <div>
+                <br>
+            <!--<i class="fad fa-chevron-double-right mr-1 text-biologia-blue fa-2x"></i>-->
+                <h4>Proyecto: "Innovación y transferencia tecnológica para el mejoramiento de la selectividad y reducción del impacto ambiental de las artes de pesca."</h4>
+                <p>
+                    Director: Raúl Alberto GONZÁLEZ <br>
+                    Co-Director: Fernando Paul OSOVNIKAR <br>
+                    Duración: 1 año <br>
+                    Objetivo General: Transferir conocimientos al sector pesquero sobre innovaciones tecnológicas 
+                    en el diseño y construcción de artes de pesca y dispositivos de selectividad, como así también 
+                    en el armado y mantenimiento de los mismos.  
+                </p>
+                <br>
+                <h4>Proyecto: "Los caracoles de la playa" </h4>
+                <p>
+                    Directora: María Soledad AVACA <br>
+                    Co-Directora: María Andrea de las Mercedes ROCHE <br>
+                    Duración: 1 año <br>
+                    Objetivo General: Acercar a los alumnos al conocimiento de uno de los grupos más comunes en el 
+                    ambiente marino, los gasterópodos, de la mano de la ciencia de una forma diferente: accesible 
+                    y divertida ampliando así sus conocimientos en el área de las ciencias naturales e integrando 
+                    dos niveles educativos diferentes. 
+                </p>
+                <br>
+                
+
+            </div>
+            <hr>
+
+        </div>
+    </div>
+</div>
