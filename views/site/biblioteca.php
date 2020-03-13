25a38ae24d1af779ffa43b6d5a6a808acdb57278
norberto <norberto@norberto>
2020-03-13 19:00:38 -0300

    vista de extension y biblioteca

---
+++ views/site/biblioteca.php
@@ -0,0 +1,193 @@
+<?php
+
+use app\assets\FrontAsset;
+
+$this->params['frontend'] = true;
+FrontAsset::register($this);
+?>
+<div class="institucional-page">
+    <div class="row " id="institucional-banner">
+
+        <div class="d-flex justify-content-center align-items-center w-100">
+            <div> 
+                <h1 class=" bg-title-blue text-light  p-3 text-center">
+                    BIBLIOTECA 
+                </h1>
+            </div>
+
+        </div>
+
+    </div>
+    <div class="container pt-5 pb-5">
+        <div>
+            <div class="bg-biologia-blue p-2 mb-3 d-flex align-items-center">
+                <h4 class="text-light mb-0">Quienes somos</h4>
+            </div>
+            <div>
+                <br>
+            <!--<i class="fad fa-chevron-double-right mr-1 text-biologia-blue fa-2x"></i>-->
+
+                <p>
+                    La Biblioteca ESCiMar, es una Biblioteca especializada quebrinda servicios de
+                    información sobre temas marinos, acuáticos y pesqueros para las actividades
+                    académicas y de investigación que se desarrollan en la Escuela Superior de
+                    Ciencias Marinas de la Universidad Nacional del Comahue.
+                    Fue creada en el año 2003 como la Biblioteca del Instituto de Biología Marina
+                    “Almirante Storni” y en en el año 2012 cuando se creó la Escuela Superior de
+                    Ciencias Marinas, la Biblioteca pasó a formar parte de la misma.
+                </p>
+                <br>
+
+
+            </div>
+        </div>
+        <hr>
+
+        <div>
+            <div class="bg-biologia-blue p-2 mb-3 d-flex align-items-center">
+                <h4 class="text-light mb-0">VISIÓN</h4>
+            </div>
+            <div>
+                <br>
+            <!--<i class="fad fa-chevron-double-right mr-1 text-biologia-blue fa-2x"></i>-->
+
+                <p>
+                    La Biblioteca ESCiMar, es la unidad de información de los recursos de
+                    información necesarios para que la comunidad académica de la Escuela Superior
+                    30de Ciencias Marinas, pueda cumplir sus objetivos en materia de docencia,
+                    estudio, aprendizaje e investigación. Su finalidad principal es facilitar el acceso y
+                    la difusión de todos los recursos de información, tanto internos como externos, a
+                    la comunidad académica de la ESCiMar y del publico en general, así como
+                    colaborar en los procesos de creación y difusión del conocimiento, contribuyendo
+                    a la formación integral de las personas.
+                </p>
+                <br>
+
+
+
+            </div>
+            <hr>
+            <div>
+                <div class="bg-biologia-blue p-2 mb-3 d-flex align-items-center">
+                    <h4 class="text-light mb-0">MISIÓN</h4>
+                </div>
+                <div>
+                    <br>
+                <!--<i class="fad fa-chevron-double-right mr-1 text-biologia-blue fa-2x"></i>-->
+
+                    <p>
+                        La misión de la Biblioteca ESCiMAr es apoyar a la comunidad universitaria en el
+                        desarrollo de la docencia, investigación y extensión, mediante la gestión de
+                        información bibliográfica, tanto impresa como digital, innovando los servicios y
+                        productos, asegurando la calidad de atención y la formación permanente de
+                        nuestros profesionales, y satisfaciendo las necesidades e intereses de información.
+                    </p>
+                    <br>
+
+
+
+                </div>
+                <hr>
+                <div>
+                    <div class="bg-biologia-blue p-2 mb-3 d-flex align-items-center">
+                        <h4 class="text-light mb-0">Objetivos:</h4>
+                    </div>
+                    <div>
+                        <br>
+                        <!--<i class="fad fa-chevron-double-right mr-1 text-biologia-blue fa-2x"></i>-->
+                        <ul>
+                            <li>Garantizar el acceso a la información en ciencias marinas y pesquerías.</li>
+                            <li>Preservar, conservar y difundir nuestro patrimonio bibliográfico.</li>
+                            <li>Capacitar a los bibliotecarios y al personal en la aplicación de las nuevas
+                                tecnologías de la comunicación e información.</li>
+
+                        </ul>
+                        <p>
+
+                        </p>
+                        <br>
+
+                    </div>
+                    <hr>
+
+                </div>
+            </div>
+            <div>
+                <div class="bg-biologia-blue p-2 mb-3 d-flex align-items-center">
+                    <h4 class="text-light mb-0">Nuestros servicios:</h4>
+                </div>
+                <div>
+                    <br>
+                <!--<i class="fad fa-chevron-double-right mr-1 text-biologia-blue fa-2x"></i>-->
+                    <ul>
+                        <li>Gestión de Información y Documentación Científica.</li>
+                        <li>Préstamo de libros a domicilio.</li>
+                        <li>Servicio de Referencia.</li>
+                        <li>Salas de Lectura.</li>
+                        <li>Digitalización de Bibliografías Especializadas.</li>
+                        <li>Acceso a Catálogo On-line.</li>
+                        <li>Confección de Bibliografías.</li>
+                        
+
+                    </ul>
+                    <p>
+
+                    </p>
+                    <br>
+
+                </div>
+                <hr>
+                <div>
+                <div class="bg-biologia-blue p-2 mb-3 d-flex align-items-center">
+                    <h4 class="text-light mb-0">Horario:</h4>
+                </div>
+                <div>
+                    <br>
+                <!--<i class="fad fa-chevron-double-right mr-1 text-biologia-blue fa-2x"></i>-->
+
+                    <p>
+                        Lunes a Viernes 08.30hs a 14.30hs
+                    </p>
+                    <br>
+                </div>
+                <hr>
+                <div>
+                <div class="bg-biologia-blue p-2 mb-3 d-flex align-items-center">
+                    <h4 class="text-light mb-0">Horario:</h4>
+                </div>
+                <div>
+                    <br>
+                <!--<i class="fad fa-chevron-double-right mr-1 text-biologia-blue fa-2x"></i>-->
+
+                    <p>
+                        Lunes a Viernes 08.30hs a 14.30hs
+                    </p>
+                    <br>
+                </div>
+                <hr>
+                <div>
+                <div class="bg-biologia-blue p-2 mb-3 d-flex align-items-center">
+                    <h4 class="text-light mb-0">Contacto:</h4>
+                </div>
+                <div>
+                    <br>
+                <!--<i class="fad fa-chevron-double-right mr-1 text-biologia-blue fa-2x"></i>-->
+
+                    <p>
+                        biblioteca.escimar@ibmp.uncoma.edu.ar <br>
+                        02934-422752 interno 106
+                    </p>
+                    <br>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
